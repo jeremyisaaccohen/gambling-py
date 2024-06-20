31f958ce5216627ef8da907684acaffa18936e16
Jeremy Cohen <jeremycohen2323@gmail.com>
2024-06-20 18:52:52 -0400

    tracking balance

--- GombocGamplingPy/gambling.py
+++ GombocGamplingPy/gambling.py
@@ -3,30 +3,31 @@ from flask import Flask, jsonify, request, render_template
 
 app = Flask(__name__)
 
+BALANCE = 1000
+
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', balance=BALANCE)
 
 # Route for placing a bet
 @app.route('/bet', methods=['POST'])
 def place_bet():
-    amount = request.form.get('amount')
-    number = request.form.get('number')
-
-    # Convert amount and number to integers
-    amount = int(amount)
-    number = int(number)
+    global BALANCE
+    data = request.get_json()
+    amount = int(data.get('amount'))
+    number = int(data.get('number'))
 
     # Simulate dice roll
     dice_roll = random.randint(1, 6)
     if number == dice_roll:
         result = 'win'
         winnings = amount * 5
+        BALANCE += winnings
+        return jsonify({'balance': BALANCE, 'result': result, 'winnings': winnings, 'dice_roll': dice_roll})
     else:
         result = 'lose'
-        winnings = 0
-
-    return jsonify({'result': result, 'winnings': winnings, 'dice_roll': dice_roll})
+        BALANCE -= amount
+        return jsonify({'balance': BALANCE, 'result': result, 'losings': amount, 'dice_roll': dice_roll})
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ jcohen-readme.md
@@ -0,0 +1,5 @@
+### Assumptions
+
+We want to be a one-of-a-kind merciful casino, and not allow gamblers to bet money they don't have!
+- `bet_amount <= balance`
+
